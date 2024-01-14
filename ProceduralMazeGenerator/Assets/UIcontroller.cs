76f107b88642002026439f51df25c1f8968880d0
Enno Kleinfeldt <enno.kleinfeldt@hotmail.de>
2024-01-14 18:26:37 +0100

    creation of UI elements
    i created the first UI elements that i will need &
    the UI manager script

---
+++ ProceduralMazeGenerator/Assets/UIcontroller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIcontroller : MonoBehaviour
+{
+    [SerializeField] private GameObject _mazeGeneratorRef;
+    [SerializeField] private Button _startButton;
+    void Update()
+    {
+        //check for changed slider or textvalue
+        //apply all values
+    }
+
+    public void OnButtonClickTEST()
+    {
+        
+    }
+}
